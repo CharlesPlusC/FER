7c173d6ddaad8223d5d174db282659242746bcf0
CharlesPlusC <c.constant14@eabjm.org>
2021-06-30 18:42:45 +0100

    Commented the code. Made variables for frame rate. Frame counter does not work properly.

--- video_to_frame_DAISEE.py
+++ video_to_frame_DAISEE.py
@@ -3,12 +3,23 @@ import glob
 import cv2
 
 # Specify the file the videos are stored in
-PathOut = r'C:/Users/chazzers/Desktop/DAiSEE/DataSet/Frames/'
+PathOut = r'D:/DAiSEE/DAiSEE/DataSet/Frames/'
 
 # Specify the file you want the frames to be stored in
-PathIn = r'C:/Users/chazzers/Desktop/DAiSEE/DataSet/Videos/'
+PathIn = r'D:/DAiSEE/DAiSEE/DataSet/Videos/'
+# changed path to D: instead of C: to test things
+# TODO: change this to correct path when we want to process all the videos
 
+# The frame rate that the film is recorded at -> Dependent on camera (usually 30)
+video_frame_rate = 30
+
+# The frame rate we want (i.e. "I want a frame every x seconds")
+required_frame_rate = 5
+
+# Making a blank array that will be populated with the full paths of all videos
 video_paths = []
+
+# Finding the name of all the video paths in the provided file structure
 
 for folder in os.listdir(PathIn):
     folder = PathIn + folder
@@ -19,19 +30,20 @@ for folder in os.listdir(PathIn):
         for video in os.listdir(vid):
             video = vid + "/" + video
         video_paths.append(video)
-# print(video_paths)
 
-vid_count = 0
-frame_count = 0
+# using OpenCV to split all the videos specified into their component frames
+vid_count = 1
+frame_count = 1
 
 for i in video_paths:
     cap = cv2.VideoCapture(i)
     success, image = cap.read()
-    vid_count += 1
+    frame_count = 1
 
     while success:
 
-        if frame_count % 250 == 0:
+        if frame_count % (video_frame_rate * required_frame_rate) == 0:
             cv2.imwrite(PathOut + 'video%d' % vid_count + 'frame%d.jpg' % frame_count, image)
             success, image = cap.read()
         frame_count += 1
+    vid_count += 1
