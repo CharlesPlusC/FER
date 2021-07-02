825197aa756f4f19fd69a26ee22553644983dc2a
CharlesPlusC <c.constant14@eabjm.org>
2021-07-02 16:44:04 +0100

    getting valence, rolling avg and starting inter-video stats

--- video_to_frame_DAISEE.py
+++ video_to_frame_DAISEE.py
@@ -2,6 +2,7 @@
 import os
 import glob
 import cv2
+import numpy as np
 import pandas as pd
 from deepface import DeepFace
 
@@ -10,45 +11,57 @@ from deepface import DeepFace
 # ----------------------------------------------------------------------------#
 # SPLITTING VIDEOS INTO FRAMES#
 
-def Split_v2f(PathIn = r'C:/Users/lizzy/OneDrive/Documents/Macbook Documents/COLLEGE/UCL/3rd year/Summer Project/DAiSEE_smol/Dataset/Videos/', PathOut = r'C:/Users/lizzy/OneDrive/Documents/Macbook Documents/COLLEGE/UCL/3rd year/Summer Project/DAiSEE_smol/Dataset/Frames/',required_frame_rate = 2 ):
+# Specify the file you want the frames to be stored in
+PathOut = r'C:/Users/chazzers/Desktop/DAiSEE_smol/DataSet/Frames/'
+# TODO: change the name of the pathout once we want to run the full thing
 
- # The frame rate that the film is recorded at -> Dependent on camera (usually 30)
- video_frame_rate = 30
+# TODO: Add an ifloop that sees if the PathOut is populated and doesnt run the splitting if it is
 
- # Making a blank array that will be populated with the full paths of all videos
- video_paths = []
+# Specify the file the videos are stored in
+PathIn = r'C:/Users/chazzers/Desktop/DAiSEE_smol/DataSet/Videos/'
+# changed path to D: instead of C: to test things
+# TODO: change this to correct path when we want to process all the videos
 
- # # Finding the name of all the video paths in the provided file structure
+# The frame rate that the film is recorded at -> Dependent on camera (usually 30)
+video_frame_rate = 30
 
- for folder in os.listdir(PathIn):
-    folder = PathIn + "/" + folder
+# The frame rate we want (i.e. "I want a frame every x seconds")
+required_frame_rate = 2
 
-    for vid in os.listdir(folder):
-        vid = folder + "/" + vid
+# Making a blank array that will be populated with the full paths of all videos
+video_paths = []
 
-        for video in os.listdir(vid):
-            video = vid + "/" + video
-        video_paths.append(video)
+# # Finding the name of all the video paths in the provided file structure
 
- # using OpenCV to split all the videos specified into their component frames
- vid_count = 1
+# for folder in os.listdir(PathIn):
+#     folder = PathIn + folder
 
- for i in video_paths:
-     cap = cv2.VideoCapture(i)
-     vid_count+=1
-     success = True
-     frame_count = 1 #reset frame count to 1 at the start of every new video
-     while success:
-         success, image = cap.read()
-         print('read a new frame:',success)
-         if frame_count %(video_frame_rate*required_frame_rate) == 0:
-             cv2.imwrite(PathOut + 'video%d' % vid_count + 'frame%d.jpg' % frame_count, image)
-         frame_count += 1
+#     for vid in os.listdir(folder):
+#         vid = folder + "/" + vid
 
-print(Split_v2f())
+#         for video in os.listdir(vid):
+#             video = vid + "/" + video
+#         video_paths.append(video)
+
+# # using OpenCV to split all the videos specified into their component frames
+# vid_count = 1
+
+# for i in video_paths:
+#     cap = cv2.VideoCapture(i)
+#     vid_count+=1
+#     success = True
+#     frame_count = 1 #reset frame count to 1 at the start of every new video
+#     while success:
+#         success, image = cap.read()
+#         print('read a new frame:',success)
+#         if frame_count %(video_frame_rate*required_frame_rate) == 0:
+#             cv2.imwrite(PathOut + 'video%d' % vid_count + 'frame%d.jpg' % frame_count, image)
+#         frame_count += 1
+
+# # TODO: make this code not end with an error
 
 
-# ----------------------------------------------------------------------------#
+# # ----------------------------------------------------------------------------#
 # PUTTING THE FRAMES THROUGH DEEPFACE AND OUTPUTTING THEM AS PD DATAFRAMES#
 
 # making a loop that takes the frames from one video at a time, puts them into an array and passes them through deepface
@@ -58,8 +71,12 @@ img_array = []
 dfs = []
 
 # takes all the photos that contain the number of 'video_counter' and puts them through deepface
-for i in range(0, len(video_paths), 1):
+# TODO: doing 10 videos for now but fix this so that it does len(all videos)
+
+# for some reason starting this loop at 0 or 1 gives me empty frames? maybe to do with the video counter starting at 1?
+for i in range(2, 10, 1):
     for filename in glob.glob(PathOut + 'video%d' % i + 'frame*.jpg'):
+        print(filename)
         # Read in the relevant images
         img = cv2.imread(filename)
         height, width, layers = img.shape
@@ -93,4 +110,43 @@ for i in range(0, len(video_paths), 1):
     df.set_index('instance', inplace=True)
     dfs.append(df)
 
+# ----------------------------------------------------------------------------#
+# TREATING THE DATA IN THE DATAFRAMES TO GET "ENGAGEMENT"
+
+# TODO: currently applying to all frames; make it so that we can split frames belonging to different individuals
+
+# Getting averages and rolling averages of positive-valence, negative-valence, and neutral emotions
+for df in dfs:
+
+    # average of negative and positive valence emotions, and neutral
+    df['neg_valence_avg'] = np.mean(df[['fear', 'disgust', 'angry', 'sad']], axis=1)
+    df['pos_valence_avg'] = np.mean(df[['happy', 'surprise']], axis=1)
+    df['neutral_avg'] = np.mean(df[['neutral']], axis=1)
+
+    # Taking a rolling average of these (length of the rolling average = 2% the length of the dataframe(or 2 frames whichever is biggest))
+    while int(len(df) * 0.02) > 1:
+        three_percent_len = int(len(df) * 0.02)
+    else:
+        three_percent_len = 1
+
+    df['neg_valence_avg_roll'] = df['neg_valence_avg'].rolling(window=three_percent_len).mean()
+    df['pos_valence_avg_roll'] = df['pos_valence_avg'].rolling(window=three_percent_len).mean()
+    df['neutral_avg_roll'] = df['neutral_avg'].rolling(window=three_percent_len).mean()
+
+    # TODO: if we want to add graphs of emotion vs. time this is the place to do it
+
+# Making a dataframe that compares all the videos to eachother (no longer computing intra-video stats but inter-video)
+
+inter_video_df = []  # empty array to add inter_video analysis data
+inter_video_df = pd.DataFrame()  # turning it into a dataframe
+
+# list of median of positive emotions for each video
+for df in dfs:
+    # list of median of pos,neg and neutral emotions for each video (one value for each video)
+    inter_video_df.append(float(df['neg_valence_avg'].median()))
+    inter_video_df.append(df['pos_valence_avg'].median())
+    inter_video_df.append(df['neutral_avg'].median())
+
+    # length of each video
+    inter_video_df.append(len(df))
 
