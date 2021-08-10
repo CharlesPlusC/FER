c17abfd56d8caf6651c18ffb6c39cd0e73c26612
Liz <lizzybates1@yahoo.co.uk>
2021-08-10 15:38:18 +0100

    cropped, greyscale images saved in class folders.

--- occlusion_test.py
+++ occlusion_test.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import os
 import cv2
+from PIL import Image
 from skimage import io, transform
 from collections import defaultdict
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -15,6 +16,8 @@ from dotenv import load_dotenv
 
 from deepface.commons import functions, realtime, distance as dst
 import pandas as pd
+from PIL import Image
+import imageio
 load_dotenv()
 # %%
 # for Charles, pip install scikit-image
@@ -33,89 +36,84 @@ def get_datagen(dataset):
               class_mode='categorical',
               batch_size=32)
 
-
-# FER_data = pd.read_csv(r"C:\Users\lizzy\OneDrive - University College London\Desktop\FER2013.csv")
-# video_paths = []
-# colour_to_grayscale = []
-
-# for folder in os.listdir(PATHOUT):
-#     folder_2 = PATHOUT + folder
-#     video_paths.append(folder_2)
-#
-# for path in video_paths:
-#     img_path = r'C:\Users\ccons\OneDrive\Desktop\DAiSEE_smol\Dataset\Frames\video32frame180.jpg'
-#     original_img = cv2.imread(img_path)
-#
-#     detector_backend = 'opencv'
-#     enforce_detection = False
-#     img, region = functions.preprocess_face(img = img_path, target_size = (48, 48), grayscale = True, enforce_detection = enforce_detection, detector_backend = detector_backend, return_region = True)
-#
-#     img.resize(48,48)
-#     colour_to_grayscale.append(img)
+img_paths = []
+for path in os.listdir(REALPATH):
+    img_folder = REALPATH + path
+    for face in os.listdir(img_folder):
+        img_path = img_folder + '/' + face
+        img_paths.append(img_path)
+for i in img_paths:
+    img_path = i
+    out_path = i + '.tiff'
+    detector_backend = 'opencv'
+    enforce_detection = False
+    img, region = functions.preprocess_face(img=img_path, target_size=(48, 48), grayscale=True,enforce_detection=enforce_detection, detector_backend=detector_backend, return_region=True)
+    img.resize(48,48)
+    cv2.imwrite(out_path,img)
+    print("image done", img)
 
 # for i in video_paths:
-#     originalImage = cv2.imread(i)
-#     greyimage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
-#     grey_array = np.asarray(greyimage)
+#     originalImage = cv2.imread(image)
+#     grey_image = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+#     grey_array = np.asarray(grey_image)
 #     colour_to_grayscale.append(grey_array)
 
 
-X_test_gen = get_datagen(REALPATH)
-
-# print(len(X_test_gen.filepaths))
-
-X_test = np.zeros((len(X_test_gen.filepaths), 48, 48, 1))
-Y_test = np.zeros((len(X_test_gen.filepaths), 7))
-for i in range(0,len(X_test_gen.filepaths)):
-  x = io.imread(X_test_gen.filepaths[i], as_gray=True) #loading the frames from the file location
-  X_test[i,:] = transform.resize(x, (48,48,1))
-  Y_test[i,X_test_gen.classes[i]] = 1
-
-#TODO Added model here in Stanford version
+# X_test_gen = get_datagen(REALPATH)
 #
-# Think it's making an array of the shape 40x40x1, full of 0.5s, then another two of smaller sizes.
-def iter_occlusion(image, size=8):
-    occlusion = np.full((size * 5, size * 5, 1), [0.5], np.float32)
-    occlusion_center = np.full((size, size, 1), [0.5], np.float32)
-    occlusion_padding = size * 2
+# X_test = np.zeros((len(X_test_gen.filepaths), 48, 48, 1))
+# Y_test = np.zeros((len(X_test_gen.filepaths), 7))
+# for i in range(0,len(X_test_gen.filepaths)):
+#   x = io.imread(X_test_gen.filepaths[i], as_gray=True) #loading the frames from the file location
+#   X_test[i,:] = transform.resize(x, (48,48,1))
+#   Y_test[i,X_test_gen.classes[i]] = 1
 #
-# Padding the array/tensor adds 0s to convert to a shape that's better for the convolution, without losing any pixel info
-    print('padding...')
-    image_padded = np.pad(image, ( \
-                        (occlusion_padding, occlusion_padding), (occlusion_padding, occlusion_padding), (0, 0) \
-                        ), 'constant', constant_values = 0.0)
-
-    for y in range(occlusion_padding, image.shape[0] + occlusion_padding, size):
-        for x in range(occlusion_padding, image.shape[1] + occlusion_padding, size):
-            tmp = image_padded.copy()
-
-            tmp[y - occlusion_padding:y + occlusion_center.shape[0] + occlusion_padding, \
-                x - occlusion_padding:x + occlusion_center.shape[1] + occlusion_padding] \
-                = occlusion
-
-            tmp[y:y + occlusion_center.shape[0], x:x + occlusion_center.shape[1]] = occlusion_center
-
-            yield x - occlusion_padding, y - occlusion_padding, \
-                  tmp[occlusion_padding:tmp.shape[0] - occlusion_padding, occlusion_padding:tmp.shape[1] - occlusion_padding]
-
-# #######
+# #TODO Added model here in Stanford version
+# #
+# # Think it's making an array of the shape 40x40x1, full of 0.5s, then another two of smaller sizes.
+# def iter_occlusion(image, size=8):
+#     occlusion = np.full((size * 5, size * 5, 1), [0.5], np.float32)
+#     occlusion_center = np.full((size, size, 1), [0.5], np.float32)
+#     occlusion_padding = size * 2
+# #
+# # Padding the array/tensor adds 0s to convert to a shape that's better for the convolution, without losing any pixel info
+#     print('padding...')
+#     image_padded = np.pad(image, ( \
+#                         (occlusion_padding, occlusion_padding), (occlusion_padding, occlusion_padding), (0, 0) \
+#                         ), 'constant', constant_values = 0.0)
 #
-i = 1
-data = X_test[i]
-correct_class = np.argmax(Y_test[i])
-
-# input tensor for model.predict
-inp = data.reshape(1,48,48,1)
-# image data for matplotlib's imshow
-img = data.reshape(48,48)
-# occlusion
-img_size = img.shape[0]
-occlusion_size = 4
-_ = plt.imshow(img,cmap='gray')
-
-# ## ADDING THE MODEL
-model = load_model('C:/Users/lizzy/OneDrive/Documents/Macbook Documents/COLLEGE/UCL/3rd year/Summer Project/DAiSEE_smol/emotionmodel.h5')
+#     for y in range(occlusion_padding, image.shape[0] + occlusion_padding, size):
+#         for x in range(occlusion_padding, image.shape[1] + occlusion_padding, size):
+#             tmp = image_padded.copy()
 #
+#             tmp[y - occlusion_padding:y + occlusion_center.shape[0] + occlusion_padding, \
+#                 x - occlusion_padding:x + occlusion_center.shape[1] + occlusion_padding] \
+#                 = occlusion
+#
+#             tmp[y:y + occlusion_center.shape[0], x:x + occlusion_center.shape[1]] = occlusion_center
+#
+#             yield x - occlusion_padding, y - occlusion_padding, \
+#                   tmp[occlusion_padding:tmp.shape[0] - occlusion_padding, occlusion_padding:tmp.shape[1] - occlusion_padding]
+#
+# # #######
+# #
+# i = 1
+# data = X_test[i]
+# correct_class = np.argmax(Y_test[i])
+#
+# # input tensor for model.predict
+# inp = data.reshape(1,48,48,1)
+# # image data for matplotlib's imshow
+# img = data.reshape(48,48)
+# # occlusion
+# img_size = img.shape[0]
+# occlusion_size = 4
+# _ = plt.imshow(img,cmap='gray')
+# cv2.imwrite(r'C:\Users\lizzy\OneDrive\Documents\Macbook Documents\COLLEGE\UCL\3rd year\Summer Project\DAiSEE_smol\Dataset\Next_Dataset\0 angry\actual_greyscale', img)
+#
+# # ## ADDING THE MODEL
+# model = load_model('C:/Users/lizzy/OneDrive/Documents/Macbook Documents/COLLEGE/UCL/3rd year/Summer Project/DAiSEE_smol/emotionmodel.h5')
+# #
 # print('occluding...')
 #
 # heatmap = np.zeros((img_size, img_size), np.float32)
