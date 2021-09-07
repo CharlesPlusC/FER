fed378b78b33b36a408d7a27bf96160cae74d844
CharlesPlusC <c.constant14@eabjm.org>
2021-09-07 09:51:13 +0100

    New plots for paper (per person and per video or 3 key metrics)

--- engagement_data_viz.py
+++ engagement_data_viz.py
@@ -87,7 +87,7 @@ def scatter_individual(yax="pos_diff", xax="Engagement", fit="lin_reg"):
     print("mean r-squared value:", statistics.mean(rsquareds))
 
 
-yax="pos_diff"
+yax="var_diff"
 xax="Engagement"
 
 LABELS = os.getenv("LABELS")  # Import DAiSEE Labels
@@ -131,30 +131,57 @@ means = with_labels.groupby("binary_eng").mean()
 
 person_avg = with_labels.groupby("NameID").mean()
 
-plt.scatter(person_avg["var_mean"], person_avg["Engagement"], alpha = 0.25)
-plt.xlim()
-plt.xlabel("Average variance per person")
-plt.ylim(-0.5,4)
-plt.ylabel("Average Engagement Score per person")
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=8)
+plt.rc('legend', fontsize = 8)
+plt.rc('axes', labelsize=8)
+plt.rcParams["figure.figsize"] = (3.5, 2.25)
+plt.rcParams.update({'font.size': 8})
+
+sns.countplot(x=with_labels["Engagement"])
+plt.savefig(r'C:\Users\chazzers\Desktop\daisee_value_count.png',dpi=1200, bbox_inches="tight")
 plt.show()
-#
+# with_labels["binary_eng"].value_counts()
+
+# plt.scatter(person_avg["var_mean"], person_avg["Engagement"], alpha = 0.25)
+# plt.xlim(0,500)
+# plt.ylim(0,4)
+# plt.ylabel("Engagement Score")
+# plt.xlabel("Mean Variance of Emotion per Person")
+# plt.savefig(r'C:\Users\chazzers\Desktop\var_eng_person_daisee.png',dpi=1200, bbox_inches="tight")
+# #
 # plt.scatter(person_avg["pos_mean"], person_avg["Engagement"], alpha = 0.25)
-# plt.xlim()
-# plt.xlabel("Average positive emotion per person")
-# plt.ylim(-0.5,4)
-# plt.ylabel("Average Engagement Score per person")
+# plt.xlim(0,20)
+# plt.xlabel("Average positive emotion per person (%)")
+# plt.ylim(0,4)
+# plt.ylabel("Engagement Score")
 # plt.show()
-
+#
 # plt.scatter(person_avg["neg_mean"], person_avg["Engagement"], alpha = 0.25)
-# plt.xlim()
-# plt.xlabel("Average negative emotion per person")
-# plt.ylim(-0.5,4)
-# plt.ylabel("Average Engagement Score per person")
+# plt.xlim(0,20)
+# plt.xlabel("Average negative emotion per person (%)")
+# plt.ylim(0,4)
+# plt.ylabel("Engagement Score")
 # plt.show()
+#
+# plt.scatter(with_labels["var_diff"], with_labels["Engagement"], alpha = 0.1)
+# # plt.xlim(-100,100)
+# plt.xlabel("Change in variance(relative to person mean)")
+# plt.ylim(0,4)
+# plt.ylabel("Engagement Score")
+# plt.savefig(r'C:\Users\chazzers\Desktop\var_eng_pervid_daisee.png',dpi=1200, bbox_inches="tight")
 
-# plt.scatter(with_labels["percent_diff_var"], with_labels["Engagement"], alpha = 0.25)
-# plt.xlim(-100,100)
-# plt.xlabel("Normalized variance")
+#
+# plt.scatter(with_labels["pos_diff"], with_labels["Engagement"], alpha = 0.015)
+# # plt.xlim(-100,100)
+# plt.xlabel("difference in positive emotion from the mean")
+# plt.ylim(-0.5,4)
+# plt.ylabel("Engagement Score")
+# plt.show()
+#
+# plt.scatter(with_labels["neg_diff"], with_labels["Engagement"], alpha = 0.015)
+# # plt.xlim(-100,100)
+# plt.xlabel("difference in negative emotion from the mean")
 # plt.ylim(-0.5,4)
 # plt.ylabel("Engagement Score")
 # plt.show()
@@ -172,52 +199,44 @@ plt.show()
 # plt.ylim(-0.5,4)
 # plt.ylabel("Engagement Score")
 # plt.show()
-
-#boxplots and stripplots
-# ax = sns.boxplot(x = with_labels["Engagement"], y= with_labels["percent_diff_var"])
-# ax = sns.stripplot(x = with_labels["Engagement"], y= with_labels["percent_diff_var"], jitter = 0.2, size = 2.5 )
+#
+# #boxplots and stripplots
+# ax = sns.swarmplot(x = with_labels["Engagement"])
+# # ax = sns.stripplot(x = with_labels["Engagement"], y= with_labels["percent_diff_var"], jitter = 0.2, size = 2.5 )
 # plt.show()
-
-##big plot with all pairwise combinations
+#
+# ##big plot with all pairwise combinations
 # sns.set()
 # sns.pairplot(with_labels)
 # plt.show()
-
-#     ClipID = []
-#     for name in df.index:
-#         ClipID.append(name[1])  # get the clip ID from the index
-#     df["ClipID"] = ClipID  # make the clip index array into a column
-#     analyse = pd.merge(df, labels, how='left', on='ClipID')  # merge the df and the scores based on 'ClipID'
 #
-#     binary_y = []
-#     for i in analyse["Engagement"]:
-#         if i >= 3:
-#             binary_y.append(1)
-#         else:
-#             binary_y.append(0)
+# binary_y = []
+# for i in person_avg["Engagement"]:
+#     if i >= 3:
+#         binary_y.append(1)
+#     else:
+#         binary_y.append(0)
 #     #add binary separation to dataframe
-#     analyse["Binary_Engagement"] = binary_y
-#     #count number of engaged and not engaged and plot them
-#     # print("Engaged = 1; Not Engaged = 0:", analyse["Binary_Engagement"].value_counts())
-#     # sns.countplot(x = binary_y, data=analyse)
+# person_avg["Binary_Engagement"] = binary_y
+#     # count number of engaged and not engaged and plot them
+#     # print("Engaged = 1; Not Engaged = 0:", with_labels["Binary_Engagement"].value_counts())
+#     # sns.countplot(x = binary_y, data=with_labels)
 #     # plt.show()
+# mean_data = person_avg.groupby("Binary_Engagement").mean()
+# var_eng = mean_data["var_diff"].iloc[0]
+# var_noteng = mean_data["var_diff"].iloc[1]
+# mean_var_eng.append(var_eng)
+# mean_var_noteng.append(var_noteng)
 #
-#     mean_data = analyse.groupby("Binary_Engagement").mean()
-#
-#     var_eng = mean_data["var_diff"].iloc[0]
-#     var_noteng = mean_data["var_diff"].iloc[1]
-#
-#     mean_var_eng.append(var_eng)
-#     mean_var_noteng.append(var_noteng)
-#
-#     eng_var_arr = np.array(mean_var_eng)
-#     noteng_var_arr = np.array(mean_var_noteng)
+# eng_var_arr = np.array(mean_var_eng)
+# noteng_var_arr = np.array(mean_var_noteng)
 #
 # bins = np.linspace(-100, 100, 30)
 # plt.hist(eng_var_arr,bins, alpha=0.5, label='emotion variance for engaged participants')
 # plt.hist(noteng_var_arr,bins, alpha=0.5, label='emotion variance for unengaged participants')
 # plt.legend()
 # plt.show()
+
 #     #observations from mean_data:
 #     #pos_diff is higher in not engaged
 #     #var_diff is higher in not engaged
